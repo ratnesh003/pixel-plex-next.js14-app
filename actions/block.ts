74b484efc9980e73b327214f111dfed9e34e02f5
Ratnesh Pasi <ratneshpasi03@gmail.com>
2024-04-21 18:49:27 +0530

    adding user page

--- actions/block.ts
+++ actions/block.ts
@@ -1,12 +1,32 @@
 "use server";
 
 import { revalidatePath } from "next/cache";
+import { RoomServiceClient } from "livekit-server-sdk";
 
+import { getSelf } from "@/lib/auth-service";
 import { blockUser, unblockUser } from "@/lib/block-service";
 
+const roomService = new RoomServiceClient(
+    process.env.LIVEKIT_API_URL!,
+    process.env.LIVEKIT_API_KEY!,
+    process.env.LIVEKIT_API_SECRET!,
+);
 
 export const onBlock = async (id: string) => {
-    const blockedUser = await blockUser(id);
+    const self = await getSelf();
+
+    let blockedUser;
+    try {
+        blockedUser = await blockUser(id);
+    } catch {
+
+    }
+
+    try {
+        await roomService.removeParticipant(self.id, id);
+    } catch {
+
+    }
 
     revalidatePath("/");
 
--- app/(browse)/[username]/page.tsx
+++ app/(browse)/[username]/page.tsx
@@ -4,7 +4,7 @@ import { getUserByUsername } from "@/lib/user-service";
 import { isFollowingUser } from "@/lib/follow-service";
 import { isBlockedByUser } from "@/lib/block-service";
 
-import { Actions } from "./_components/actions";
+import { StreamPlayer } from "@/components/stream-player";
 
 interface UserPageProps {
   params: {
@@ -15,7 +15,7 @@ interface UserPageProps {
 const UserPage = async ({ params }: UserPageProps) => {
   const user = await getUserByUsername(params.username);
 
-  if (!user) {
+  if (!user || !user.stream) {
     notFound();
   }
 
@@ -27,13 +27,11 @@ const UserPage = async ({ params }: UserPageProps) => {
   }
 
   return (
-    <div className="flex flex-col gap-y-4">
-      <p>User: {user.username}</p>
-      <p>UserID: {user.id}</p>
-      <p>isFollowing: {`${isFollowing}`}</p>
-      <p>is blocked by this user: {`${isBlocked}`}</p>
-      <Actions userId={user.id} isFollowing={isFollowing} />
-    </div>
+    <StreamPlayer 
+      user={user}
+      stream={user.stream}
+      isFollowing={isFollowing}
+    />
   );
 };
 
