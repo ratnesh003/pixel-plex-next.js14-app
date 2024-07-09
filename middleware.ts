17dc93181144977e0b1ff4a8aa79028b8ec0ca46
Ratnesh Pasi <ratneshpasi03@gmail.com>
2024-07-10 00:52:00 +0530

    middleware check

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,8 @@ export default authMiddleware({
   publicRoutes: [
     '/',
     '/api/webhooks(.*)',
+    '/api/webhooks/clerk',
+    '/api/webhooks/livekit',
     '/api/uploadthing',
     '/:username',
     '/search'
